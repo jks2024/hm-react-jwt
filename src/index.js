c04febcce987f55834829727677a1e0ece86db13
jks2024 <jks2024@gmail.com>
2026-06-01 10:47:07 +0900

    MODAL2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { ModalProvider } from "./context/ModalContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // 로그인 상태 전역 공유
   <AuthProvider>
-    <App />
+    <ModalProvider>
+      <App />
+    </ModalProvider>
   </AuthProvider>,
 );
